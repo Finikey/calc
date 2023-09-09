f3747917f57273d1907e510098efa341fc25bef1
finikey <finikeyshow@gmail.com>
2023-09-09 13:12:11 +0300

    new

--- js/main.js
+++ js/main.js
@@ -2,25 +2,42 @@ const input1 = document.querySelector('.input1')
 const input2 = document.querySelector('.input2')
 const plusBtn = document.querySelector('.plus')
 const minusBtn = document.querySelector('.minus')
+const delenieBtn = document.querySelector('.delenie')
+const umnojBtn = document.querySelector('.umnoj')
 const submitBtn = document.querySelector('.submitBtn')
-const resultBtn = document.querySelector('.result')
+const resultEL = document.querySelector('.result')
 
 let action = '+'
 
-plusBtn.onclick = function(){
+plusBtn.onclick = function () {
     action = '+'
 }
 
 minusBtn.onclick = function(){
     action = '-'
 }
-    
+
+delenieBtn.onclick = function(){
+    action = '/'
+}
+
+umnojBtn.onclick = function(){
+    action = '*'
+}
+
 submitBtn.onclick = function(){
     if(action === '+'){
-        const sum = Number(input1.value)+Number(input2.value)
+        const sum = Number(input1.value) + Number(input2.value)
         resultEL.textContent = sum 
-    }else if(action==='-'){
-        const sum = Number(input1.value)-Number(input2.value)
+    }else if(action === '-'){
+        const sum = Number(input1.value) - Number(input2.value)
         resultEL.textContent = sum 
+    }else if(action === '*'){
+        const sum = Number(input1.value) * Number(input2.value)
+        resultEL.textContent = sum 
+    }else if(action === '/'){
+        const sum = Number(input1.value) / Number(input2.value)
+        resultEL.textContent = sum
     }
+
 }
